fcb7231ec3614fd8f8a19a1e31ad0725429eb4ff
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-08-27 21:34:03 -0700

    renamed project file

---
+++ src/CodingTrackerApplication/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using Spectre.Console;
+
+namespace CodingTrackerApplication;
+internal class MainMenu
+{
+    internal static void GetUserInput()
+    {
+        Console.Clear();
+        bool closeApp = false;
+        while (closeApp == false)
+        {
+            AnsiConsole.Markup("[underline red]MAIN MENU[/]");
+            Console.WriteLine("\nWhat would you like to do?");
+            AnsiConsole.Markup("\nType 0 to [underline blue]Close[/]  Application.");
+            AnsiConsole.Markup("\nType 1 to [underline blue]View[/]  All Records.");
+            AnsiConsole.Markup("\nType 2 to [underline blue]Insert[/]  Record.");
+            AnsiConsole.Markup("\nType 3 to [underline blue]Delete[/]  Record.");
+            AnsiConsole.Markup("\nType 4 to [underline blue]Update[/]  Record.");
+            Console.WriteLine("\n------------------------------------------------------\n");
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "0":
+                    Console.WriteLine("\nGoodbye!\n");
+                    closeApp = true;
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    AppEngine.GetAllRecords();
+                    break;
+                case "2":
+                    AppEngine.InsertTime();
+                    break;
+                case "3":
+                    AppEngine.Delete();
+                    break;
+                case "4":
+                    AppEngine.Update();
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid Command. Please type a number from 0 to 4.\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/CodingTrackerApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Configuration;
+
+namespace CodingTrackerApplication;
+public static class Program
+{
+    static readonly string? connectionString = ConfigurationManager.AppSettings.Get("connectionString");
+    public static void Main(string[] args)
+    {
+        CreateDatabase();       
+
+        MainMenu.GetUserInput();
+    }
+    private static void CreateDatabase()
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText =
+                @"CREATE TABLE IF NOT EXISTS coding_session (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,                        
+                        StartTime TEXT,
+                        EndTime TEXT,
+                        Duration INTEGER
+                        )";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
